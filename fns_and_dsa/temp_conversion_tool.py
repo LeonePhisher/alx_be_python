9ff955d36abbb619e7c82b9ee1dfa9dbc57ce550
LeonePhisher <thackme39@gmail.com>
2025-09-26 17:44:54 +0000

    temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,5 +1,5 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
     """
@@ -31,4 +31,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
